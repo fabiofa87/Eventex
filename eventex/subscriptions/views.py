d625d0d9d01505daa43b7acaa4bd5ddd2a729af5
fabiofa87 <fabiofadev87@gmail.com>
2022-09-09 14:43:19 -0300

    alinhado com sender identity

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -15,8 +15,8 @@ def subscribe(request):
             mail.send_mail(
                 'Confirmação de inscrição',
                 body,
-                'contato@eventex.com.br',
-                ['contato@eventex.com.br', form.cleaned_data['email']]
+                'fabiofa87@gmail.com',
+                ['fabiofa87@gmail.com', form.cleaned_data['email']]
             )
             messages.success(request, 'Inscrição realizada com sucesso!')
             return HttpResponseRedirect('/inscricao/')
