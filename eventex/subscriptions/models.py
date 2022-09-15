08bcf282746159f342031bf110e7893c7d808200
fabiofa87 <fabiofadev87@gmail.com>
2022-09-15 19:11:32 -0300

    criado campo paid

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+
 class Subscription(models.Model):
     name = models.CharField('nome', max_length=100)
     cpf = models.CharField('cpf', max_length=11)
     email = models.EmailField('e-mail')
     phone = models.CharField('telefone', max_length=20)
     created_at = models.DateTimeField('criado em', auto_now_add=True)
+    paid = models.BooleanField('pago', default=False)
 
     class Meta:
         verbose_name_plural = 'inscrições'
